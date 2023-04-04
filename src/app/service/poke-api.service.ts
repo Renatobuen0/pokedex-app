7d44f165af2fb27db239d1a040e9c906d729032f
Renato Bueno <renato.bueno@performait.com>
2023-04-04 19:36:38 -0300

    Change quantity to return

--- src/app/service/poke-api.service.ts
+++ src/app/service/poke-api.service.ts
@@ -3,39 +3,30 @@ import { HttpClient } from '@angular/common/http';
 
 //Observable
 import { Observable } from 'rxjs';
-import { map, tap } from "rxjs/operators";
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokeApiService {
+  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=400';
 
-  private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100';
+  constructor(private http: HttpClient) {}
 
-  constructor(
-    private http: HttpClient
-  ) { }
-
-  get apiListAllPokemons():Observable<any>{
+  get apiListAllPokemons(): Observable<any> {
     return this.http.get<any>(this.url).pipe(
-      tap( res => res ),
-      tap( res => {
-        res.results.map( (resPokemons: any) => {
-
+      tap((res) => res),
+      tap((res) => {
+        res.results.map((resPokemons: any) => {
           this.apiGetPokemon(resPokemons.url).subscribe(
-            res => resPokemons.status = res
+            (res) => (resPokemons.status = res)
           );
-
-        })
+        });
       })
-    )
+    );
   }
 
-  public apiGetPokemon( url: string ):Observable<any>{
-    return this.http.get<any>( url ).pipe(
-      map(
-        res => res
-      )
-    )
+  public apiGetPokemon(url: string): Observable<any> {
+    return this.http.get<any>(url).pipe(map((res) => res));
   }
 }
